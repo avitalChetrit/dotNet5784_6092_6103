09bfa1b95692d452a753fe8dc645108ac2753d6d
YafitA <Yafitta5@gmail.com>
2023-12-26 20:45:19 +0200

    I am here also!

---
+++ Stage0/Program6103.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collection.Generic;
+
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome6103()
+        {
+            global::System.Console.WriteLine("I am also here!");
+        }
+    }
+}
+
+
