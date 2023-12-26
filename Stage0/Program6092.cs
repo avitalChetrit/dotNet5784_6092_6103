e36647890b87ad8d354a9b14bae2dfcca27f955b
avitalChetrit <a0587281812@gmail.com>
2023-12-26 18:57:04 +0200

    Initial code + Welcome functions

---
+++ Stage0/Program6092.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6092();
+            Welcome6103();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome6103();
+        private static void Welcome6092()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            //Console.WriteLine("{0}, welcome to my first console aplication",userName);
+            Console.WriteLine(userName + ", welcome to my first console aplication ");
+        }
+        
+    }
+}
+
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        Console.WriteLine("Hello, World!");
+//    }
+//}
