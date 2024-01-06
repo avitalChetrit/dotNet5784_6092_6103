7ad97c89d21f83ce909c7e68138d7293077104fd
YAFITTA5@GMAIL.COM <YAFITTA5@GMAIL.COM>
2024-01-07 01:10:45 +0200

    fix in Dep summary

--- DalFacade/DO/Dependency.cs
+++ DalFacade/DO/Dependency.cs
@@ -3,8 +3,8 @@
 /// Dependency Entity represents a Dependency with all its props
 /// </summary>
 /// <param name="Id">Personal random ID of the Dependency </param>
-/// <param name="preTask">Name of the previous Task</param>
-/// <param name="currTask">Name of the current Task</param>
+/// <param name="preTask">ID of the previous Task</param>
+/// <param name="currTask">ID of the current Task</param>
 
 public record Dependency
 (
