154c6f9c64ef6e75461bf7baa017af263821690f
YAFITTA5@GMAIL.COM <YAFITTA5@GMAIL.COM>
2024-01-08 19:48:59 +0200

    Shalav 2.1

--- DalFacade/DalApi/IChef.cs
+++ DalFacade/DalApi/IChef.cs
@@ -2,11 +2,5 @@
 using DO;
 
 
-public interface IChef
-{
-    int Create(Chef item); //Creates new entity object in DAL
-    Chef? Read(int id); //Reads entity object by its ID 
-    List<Chef> ReadAll(); //stage 1 only, Reads all entity objects
-    void Update(Chef item); //Updates entity object
-    void Delete(int id); //Deletes an object by its Id
-}
+public interface IChef : ICrud<Chef> { }
+
