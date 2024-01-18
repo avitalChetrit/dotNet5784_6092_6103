cb1b8f8abc23b6ff96f3933a5a1fe3502599ea12
YafitA <YAFITTA5@GMAIL.COM>
2024-01-18 15:06:21 +0200

    TaksImplemantation

--- DalXml/TaskImplementation.cs
+++ DalXml/TaskImplementation.cs
@@ -1,5 +1,7 @@
-﻿using DalApi;
+﻿using Task = DO.Task;
+using DalApi;
 using DO;
+using System.Data.Common;
 
 namespace Dal;
 
@@ -7,33 +9,66 @@ internal class TaskImplementation : ITask
 {
     readonly string s_tasks_xml = "tasks";
 
-    public int Create(DO.Task item)
+    public int Create(Task item)
     {
-        throw new NotImplementedException();
+        List<Task> Tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);  //Load
+
+        //for entities with auto id
+        int id = Config.NextTaskId;
+        Task copy = item with { Id = id };
+        Tasks.Add(copy);
+
+        XMLTools.SaveListToXMLSerializer(Tasks, s_tasks_xml);  //save
+
+        return id;//return id
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        throw new DalDeletionImpossible("Can't delete the Task object!");
     }
 
-    public DO.Task? Read(int id)
+    public Task? Read(int id)
     {
-        throw new NotImplementedException();
+        List<Task> Tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);  //Load
+        return Tasks.FirstOrDefault(item => item.Id == id);
     }
 
-    public DO.Task? Read(Func<DO.Task, bool> filter)
+    public Task? Read(Func<Task, bool> filter)
     {
-        throw new NotImplementedException();
+        List<Task> Tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);  //Load
+        return Tasks.FirstOrDefault(item => filter(item));
     }
 
-    public IEnumerable<DO.Task?> ReadAll(Func<DO.Task, bool>? filter = null)
+    public IEnumerable<Task?> ReadAll(Func<Task, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        List<Task> Tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);  //Load
+        if (filter != null)
+        {
+            return from item in Tasks
+                   where filter(item)
+                   select item;
+        }
+        return from item in Tasks
+               select item;
     }
 
-    public void Update(DO.Task item)
+    public void Update(Task item)
     {
-        throw new NotImplementedException();
+        List<Task> Tasks = XMLTools.LoadListFromXMLSerializer<Task>(s_tasks_xml);  //Load
+
+        bool isExist = Tasks.Exists(x => x.Id == item.Id);//checks if there is an objects with the same id on list
+        if (isExist)//such item is on list
+        {
+            Task dep = Tasks.Find(x => x.Id == item.Id)!;//finds the object with the same id
+            Tasks.Remove(dep);//removes the old objects
+            Tasks.Add(item);//add the new object
+        }
+        else//such item is not on list
+        {
+            throw new DalDoesNotExistException($"Task with ID={item.Id} does Not exist");
+        }
+
+        XMLTools.SaveListToXMLSerializer(Tasks, s_tasks_xml);  //save
     }
 }
