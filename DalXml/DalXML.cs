c67774a24c0722594b0f593ca070b149245967c4
YafitA <YAFITTA5@GMAIL.COM>
2024-01-18 13:23:13 +0200

    STAGE 3.6

---
+++ DalXml/DalXML.cs
@@ -0,0 +1,10 @@
+﻿using DalApi;
+namespace Dal;
+
+//stage 3
+sealed public class DalXml : IDal
+{
+    public IChef Chef => new ChefImplementation();
+    public ITask Task => new TaskImplementation();
+    public IDependency Dependency => new DependencyImplementation();
+}
