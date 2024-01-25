5ffdeb68d3ab141f428a280716ced5cebe8bd71b
avitalChetrit <a0587281812@gmail.com>
2024-01-25 15:30:22 +0200

    Bo classes- stage 4.6.1

--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -1,11 +1,22 @@
 ﻿namespace BO;
 
-//public enum ChefExperience
-//{
-//    Beginner,
-//    AdvancedBeginner,
-//    Intermediate,
-//    Advanced,
-//    Expert
-//}
+public enum ChefExperience
+{
+    Beginner,
+    AdvancedBeginner,
+    Intermediate,
+    Advanced,
+    Expert
+}
+
+/// <summary>
+/// 
+/// </summary>
+public enum ChefSpeciality
+{
+    Baker,
+    Meat,
+    SeaFood,
+    Dairy
+}
 
