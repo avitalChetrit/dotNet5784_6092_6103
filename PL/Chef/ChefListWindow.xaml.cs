7de8edbebcbd554986ad6610206e5f6db73ec45c
YafitA <YAFITTA5@GMAIL.COM>
2024-02-12 18:55:14 +0200

    Stage 5 mid

---
+++ PL/Chef/ChefListWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.Chef
+{
+    /// <summary>
+    /// Interaction logic for ChefListWindow.xaml
+    /// </summary>
+    public partial class ChefListWindow : Window
+    {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+        public IEnumerable<BO.Chef> ChefList
+        {
+            get { return (IEnumerable<BO.Chef>)GetValue(ChefListProperty); }
+            set { SetValue(ChefListProperty, value); }
+        }
+
+        public static readonly DependencyProperty ChefListProperty =
+        DependencyProperty.Register("CourseList", typeof(IEnumerable<BO.Chef>), typeof(ChefListWindow), new PropertyMetadata(null));
+        public ChefListWindow()
+        {
+            InitializeComponent();
+            ChefList = s_bl?.Chef.ReadAll()!;
+
+        }
+
+        
+
+        
+    }
+}
---
+++ PL/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using PL.Chef;
+using System.Windows;
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChefs_Click(object sender, RoutedEventArgs e)
+        {
+            new ChefListWindow().Show();
+        }
+        private void btnInitDB_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Would you like to initialize?");
+            MessageBoxResult result= MessageBox.Show("Would you like to initialize?","Initialazion Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            //if( MessageBox.Show(MessageBoxButtons.YesNo)    ==  MessageBoxResult.Yes)
+
+            //MessageBox.Show(@"Do you want to confirm?",
+            //           "Problem!",
+            //            MessageBoxButtons.YesNo);
+
+            DalTest.Initialization.Do();
+        }
+        
+    }
+}
