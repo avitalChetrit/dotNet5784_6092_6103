8c5e1bf945877bb5874dddfcbd617a8d646a6c60
YafitA <YAFITTA5@GMAIL.COM>
2024-02-17 18:45:25 +0200

    chefwindow fix

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -20,7 +20,6 @@ namespace PL
         }
         private void btnInitDB_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Would you like to initialize?");
             MessageBoxResult result= MessageBox.Show("Would you like to initialize?","Initialazion Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
             if (result==MessageBoxResult.Yes) // if the user want to initial
