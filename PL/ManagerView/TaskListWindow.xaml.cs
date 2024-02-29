8d466fc9c6c8d9e235f24d37cc461bb4b10bf93d
YafitA <YAFITTA5@GMAIL.COM>
2024-02-29 18:18:51 +0200

    changes

--- PL/ManagerView/TaskListWindow.xaml.cs
+++ PL/ManagerView/TaskListWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,7 +22,25 @@ namespace PL.ManagerView
     public partial class TaskListWindow : Window
     {
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+        //
+        
+        //ComboBoxes selectedValues
+        public BO.Status Status { get; set; } = BO.Status.All;
+        public BO.ChefExperienceFilter Complexity { get; set; } = BO.ChefExperienceFilter.All;
+        public string ChefName { get; set; } = "All";
 
+        /// <summary>
+        /// List of the names of the chef to bind to combobox
+        /// </summary>
+        public IEnumerable<String> ChefNamesList
+        {
+            get { return (IEnumerable<String>)GetValue(ChefNamesListProperty); }
+            set { SetValue(ChefNamesListProperty, value); }
+        }
+        public static readonly DependencyProperty ChefNamesListProperty =
+        DependencyProperty.Register("ChefNamesList", typeof(IEnumerable<String>), typeof(TaskListWindow), new PropertyMetadata(null));
+
+        //List of tasks shown on screen
         public IEnumerable<BO.Task> TaskList
         {
             get { return (IEnumerable<BO.Task>)GetValue(TaskListProperty); }
@@ -29,11 +48,25 @@ namespace PL.ManagerView
         }
         public static readonly DependencyProperty TaskListProperty =
         DependencyProperty.Register("TaskList", typeof(IEnumerable<BO.Task>), typeof(TaskListWindow), new PropertyMetadata(null));
-       
+    
         public TaskListWindow()
         {
             InitializeComponent();
             TaskList= s_bl.Task.ReadAll();
+            ChefNamesList = s_bl.Chef.ReadAll().Select(x => x.Name).Concat(new[] { "All" })!;
+        }
+
+        private void ChangeFilter(object sender, SelectionChangedEventArgs e)
+        {
+            TaskList = s_bl?.Task.ReadAll().Where(t =>
+             (Status == BO.Status.All || t.Status == Status) &&
+              (Complexity == BO.ChefExperienceFilter.All || t.Complexity == (BO.ChefExperience)Complexity) &&
+              (ChefName == "All" || t.Chef?.Name == ChefName))!;
+        }
+
+        private void UpdateTaskDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            new TaskWindow().Show();
         }
     }
 }
