5562f80cb6d632fc96ff4ac6d392a0db9e995bee
avitalChetrit <a0587281812@gmail.com>
2024-03-03 22:19:32 +0200

    ChefLogIn

---
+++ PL/ChefView/LogInChefWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.ChefView
+{
+    /// <summary>
+    /// Interaction logic for LogInChefWindow.xaml
+    /// </summary>
+    public partial class LogInChefWindow : Window
+    {
+        public LogInChefWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
