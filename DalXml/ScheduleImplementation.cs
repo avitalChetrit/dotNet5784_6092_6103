6a6bd62a3c9464a07247b7445fb339bb98e3b627
avitalChetrit <a0587281812@gmail.com>
2024-03-12 12:39:02 +0200

    שש

---
+++ DalXml/ScheduleImplementation.cs
@@ -0,0 +1,21 @@
+﻿using DalApi;
+using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+
+internal class ScheduleImplementation
+{
+    readonly string s_schedule_xml = "schedule";
+
+    public void Update()
+    {
+        List<string> dateList = new List<string>();
+        dateList.Add(Schedule.StartDate.ToString());
+        XMLTools.SaveListToXMLSerializer(dateList, s_schedule_xml);  //save
+    }
+}
